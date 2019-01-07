fdff52ee67403d5b110b0853d66881b7b2041a24
atilacaetano <atilacaetano95@hotmail.com>
2019-01-07 17:02:42 -0300

    update

---
+++ js/jqporti.js
@@ -0,0 +1,15 @@
+/*$(document).ready(function(){
+    $('#design').mouseenter(function(){
+        $('#photo').effect('slide',3000);
+    });
+})(jQuery);
+
+$(document).ready(function(){
+    $('#artedig').effect('slide',3000);    
+});*/
+
+$(document).ready(function(){
+	$("#tela").click(function(){
+	    $(this).hide();
+	});
+});
